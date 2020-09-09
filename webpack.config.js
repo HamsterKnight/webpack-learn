6efabd2c2ac1e72889107f35a241d24bca7ce065
zengshixin <zengshixin@moyi365.com>
2020-09-09 17:04:32 +0800

    fix: html-wbapck-plugin自动生成html模板文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
   output: {
     filename: 'main-[hash:5].js'
   },
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({
+    filename: 'test.html',
+    template: 'src/template/index.html'
+  })]
 }
--- webpack.md
+++ webpack.md
@@ -1,3 +1,3 @@
-# clean-webpack-plugin
-> 清除生成目录下的所有文件
-> 通过引入该包中的第三方 中暴露的CleanWebpackPlugin的构造函数来进行目录的清除
+# html-webpack-plugin
+> 可以生成一个html模板，自动引入打包的js文件
+> 也可以以某个文件未模板，生成html文件，然后引入打包的js文件
