aa67a67a60e780eedf88458d3fce7d47d862805d
zengshixin <zengshixin@moyi365.com>
2020-09-09 17:35:51 +0800

    fix: copy-webpack-plugin 复制静态目录文件到打包文件中

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 module.exports = {
   mode: 'development',
   output: {
     filename: 'main-[hash:5].js'
   },
   plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({
-    filename: 'test.html',
-    template: 'src/template/index.html'
+    template: './src/public/index.html'
+  }), new CopyPlugin({
+    patterns:[{
+      from:'./src/public',//将public下的内容复制到dist中, 如果复制中存在模板文件，那么HtmlWebpackPlugin生成的模板还是会覆盖该模板文件，保持对打包内容的引入
+      to: './'// 这里默认是导出到dist
+    }]
   })]
 }
--- webpack.md
+++ webpack.md
@@ -1,3 +1,3 @@
-# html-webpack-plugin
-> 可以生成一个html模板，自动引入打包的js文件
-> 也可以以某个文件未模板，生成html文件，然后引入打包的js文件
+# copy-webpack-plugin
+> 将相关静态目录文件，复制到打包文件夹中
+> https://webpack.docschina.org/plugins/copy-webpack-plugin/#root
