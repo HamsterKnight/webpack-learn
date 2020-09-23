8397b933442fa4a9261a879e5527a58a68f9b72b
zengshixin <zengshixin@moyi365.com>
2020-09-23 18:11:09 +0800

    build: webpack内置插件

--- src/a.js
+++ src/a.js
@@ -1,6 +1,5 @@
 var imgUrl = require('./public/assets/webpack.jpg') // 这里获取到的是一个地址
-console.log(imgUrl)
-var img = document.createElement('img')
-img.src = imgUrl.default
-document.body.appendChild(img)
+console.log('version', version)
+var img = $(`<img src="${imgUrl.default}">`)
+$('body').append(img)
 module.exports = 'a'
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
 module.exports = {
   mode: 'development',
   output: {
     filename: 'script/main-[hash:5].js',
-    publicPath: '/'
+    publicPath: '../'
   },
   module: {
     rules:[
@@ -20,12 +20,28 @@ module.exports = {
       }
     ]
   },
-  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({
-    template: './src/public/index.html',
-    filename: "html/index.html"
-  })],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      version: '1'
+    }),
+    new webpack.BannerPlugin({
+      banner: `
+        author:zengshixin,
+        data: 2020-09-23
+      `
+    }),
+    new webpack.ProvidePlugin({
+      '$': 'jquery'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/public/index.html',
+      filename: "html/index.html"
+    })
+  ],
   devServer: {
     open: true, // 配置运行项目，自动打开浏览器,
+    openPage: '/html/index.html',
     port: '8081', // 配置打开端口，
     proxy: {
       '/api': {
